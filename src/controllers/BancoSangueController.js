d2352436707b31b6205c61467ede061fc6a7de88
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-04-03 23:53:00 -0300

    fix details

--- src/controllers/BancoSangueController.js
+++ src/controllers/BancoSangueController.js
@@ -2,7 +2,11 @@ const database = require('../database/database')
 
 module.exports = {
     async indexAll(req, res) {
+        
+        const [count] = await database('tbBancoSangue').count()
         const result = await database('tbBancoSangue').select('*')
+
+        res.header('X-Total-Count', count['count(*)'])
         return res.json(result)
     },
 
