a7415d14cfe14064d6164abd5c93adc3a50d53c8
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-04-03 23:54:14 -0300

    implementação da listagem de doadores específicos

---
+++ src/controllers/DoadoresController.js
@@ -0,0 +1,39 @@
+const database = require('../database/database')
+const { dateFormat } = require('../lib/dateFormat')
+
+module.exports = {
+    async index(req, res) { 
+        const { id } = req.params
+        
+        const result = await database('tbDoador')
+                        .where('idUsuario', id)
+                        .join('tbResponsavel', {
+                            'tbResponsavel.idResponsavel': 'tbDoador.idResponsavel' 
+                        })
+                        .join('tbSexo', {
+                            'tbSexo.idSexo': 'tbDoador.idSexo'
+                        })
+                        .join('tbFatorRh', {
+                            'tbFatorRh.idFatorRh': 'tbDoador.idFatorRh'
+                        })
+                        .join('tbTipoSanguineo', {
+                            'tbTipoSanguineo.idTipoSanguineo': 'tbDoador.idTipoSanguineo'
+                        })
+                        .select([
+                            'tbDoador.*',
+                            'tbResponsavel.nomeResponsavel',
+                            'tbResponsavel.cpfResponsavel',
+                            'tbResponsavel.rgResponsavel',
+                            'tbResponsavel.dataNascimentoResponsavel',
+                            'tbSexo.descricaoSexo',
+                            'tbFatorRh.descricaoFatorRh',
+                            'tbTipoSanguineo.descricaoTipoSanguineo'
+                        ])
+                        .first()
+
+        result.dataNascimentoResponsavel = await dateFormat(result.dataNascimentoResponsavel)
+        result.dataNascimentoDoador = await dateFormat(result.dataNascimentoDoador)
+
+        return res.json(result)
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ const database = require('./database/database')
 
 const LoginController = require('./controllers/LoginController')
 const BancoSangueController = require('./controllers/BancoSangueController')
+const DoadoresController = require('./controllers/DoadoresController')
+
+routes.post('/login', LoginController.validate)
 
 routes.get('/bancos', BancoSangueController.indexAll)
 routes.get('/bancos/:id', BancoSangueController.index)
 
-routes.post('/login', LoginController.validate)
+routes.get('/doadores/:id', DoadoresController.index)
 
 module.exports = routes
