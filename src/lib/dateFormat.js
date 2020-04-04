c102fe40a74873d8b1cc95e883afff8bce1e9145
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-04-03 23:53:39 -0300

    promise para formatação de datas

---
+++ src/lib/dateFormat.js
@@ -0,0 +1,18 @@
+module.exports = {
+    async dateFormat(date) {
+        let day = date.getDate()
+        let month = date.getMonth() + 1
+        let year = date.getFullYear()
+    
+        if(day.toString().length == 1)
+            day = '0' + day
+        if(month.toString().length == 1)
+            month = '0' + month
+    
+        let newdate = day + 
+                '/' + month + 
+                '/' + year
+    
+        return newdate.toString()
+    }
+}
