a56f5f093b6a8eb6f9b5a6d7db37b91bd672f094
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-05-06 06:03:39 -0300

    atualizando o serviço de e-mail

--- src/services/sendmailer/SendMailer.js
+++ src/services/sendmailer/SendMailer.js
@@ -12,15 +12,31 @@ module.exports = {
             }
         })
 
+        const remetenteEmail = (req.body.remetente) 
+        ? req.body.remetenteEmail : process.env.SENDMAILER_USER
+        
+        const remetenteNome = (req.body.remetenteNome) 
+        ? req.body.remetenteNome : 'Horizon'
+
+
+        const email = `<strong> Nome: ${ remetenteNome } </strong> <br> 
+                       <strong> E-mail: ${ remetenteEmail } </strong> <br> 
+                       <article> ${ req.body.texto } </article>`
+
+        
         transporter.sendMail({
             from: `Needy <${process.env.SENDMAILER_USER}>`,
-            to: req.body.to,
-            subject: req.body.subject,
-            html: req.body.text
+            to: req.body.para,
+            subject: req.body.assunto,
+            html: email
         }).then(message => {
-            return res.json({ success: true })
+            return res.json({
+                success: true
+            })
         }).catch(err => {
-            return res.json({ success: false })
+            return res.json({
+                success: false
+            })
         })
 
     }
