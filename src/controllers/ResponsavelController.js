f06997db11f71bb2785bc59901d89d4ba363fdda
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-07-03 23:47:15 -0300

    put the number of responsavel no json

--- src/controllers/ResponsavelController.js
+++ src/controllers/ResponsavelController.js
@@ -5,14 +5,22 @@ module.exports = {
   async index(req, res) {
     const { id } = req.params;
 
-    const result = await database("tbResponsavel")
+    let result = await database("tbResponsavel")
       .where("idResponsavel", id)
       .select("*")
+      .first();
+
+    let numeroResponsavel = await database("tbTelefoneResponsavel")
+      .where("idResponsavel", id)
+      .select("numeroTelefoneResponsavel")
       .first();
 
     result.dataNascimentoResponsavel = dateFormat(
       result.dataNascimentoResponsavel
     );
+
+    result.telefone = numeroResponsavel;
+
     return res.status(200).json(result);
   },
 };
