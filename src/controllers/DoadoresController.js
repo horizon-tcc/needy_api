76514f693706b6fb4c716f2e52954e30079ea4e7
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-07-05 03:54:22 -0300

    bugfix

--- src/controllers/DoadoresController.js
+++ src/controllers/DoadoresController.js
@@ -1,42 +1,63 @@
-const database = require("../database/database");
-const { dateFormat } = require("../utils/dateFormat");
+const database = require('../database/database')
+const {
+  dateFormat
+} = require('../utils/dateFormat')
 
 module.exports = {
   async index(req, res) {
-    const { id } = req.params;
+    const {
+      id
+    } = req.params
 
-    let result = await database("tbDoador")
-      .where("idUsuario", id)
-      .join("tbSexo", {
-        "tbSexo.idSexo": "tbDoador.idSexo",
+    const result = await database('tbDoador')
+      .where('idUsuario', id)
+      .join('tbSexo', {
+        'tbSexo.idSexo': 'tbDoador.idSexo',
       })
-      .join("tbFatorRh", {
-        "tbFatorRh.idFatorRh": "tbDoador.idFatorRh",
+      .join('tbFatorRh', {
+        'tbFatorRh.idFatorRh': 'tbDoador.idFatorRh',
       })
-      .join("tbTipoSanguineo", {
-        "tbTipoSanguineo.idTipoSanguineo": "tbDoador.idTipoSanguineo",
+      .join('tbTipoSanguineo', {
+        'tbTipoSanguineo.idTipoSanguineo': 'tbDoador.idTipoSanguineo',
+      })
+      .join('tbTelefoneDoador', {
+        'tbTelefoneDoador.idDoador': 'tbDoador.idDoador'
       })
       .select([
-        "tbDoador.*",
-        "tbSexo.descricaoSexo",
-        "tbFatorRh.descricaoFatorRh",
-        "tbTipoSanguineo.descricaoTipoSanguineo",
+        'tbDoador.*',
+        'tbSexo.descricaoSexo',
+        'tbFatorRh.descricaoFatorRh',
+        'tbTipoSanguineo.descricaoTipoSanguineo',
+        'tbTelefoneDoador.numeroTelefoneDoador'
       ])
-      .first();
+      .first()
 
-    let numeroDoador = await database("tbTelefoneDoador")
-      .where("idDoador", id)
-      .select("numeroTelefoneDoador");
+    const phoneNumbers = await database('tbTelefoneDoador')
+      .where('idDoador', id)
+      .pluck('tbTelefoneDoador.numeroTelefoneDoador')
 
-    result.dataNascimentoDoador = dateFormat(result.dataNascimentoDoador);
+    result.numeroTelefoneDoador = phoneNumbers
+    result.dataNascimentoDoador = dateFormat(result.dataNascimentoDoador)
 
-    result.telefones = numeroDoador;
 
-    return res.status(200).json(result);
+    return res.status(200).json(result)
   },
 
   async indexAll(req, res) {
-    const result = await database("tbDoador").select("*");
-    return res.status(200).json(result);
+    const result = await database('tbDoador')
+      .select('*')
+
+    for (const doador of result) {
+
+      const phoneNumbers = await database('tbTelefoneDoador')
+        .where('idDoador', doador.idDoador)
+        .pluck('tbTelefoneDoador.numeroTelefoneDoador')
+
+      doador.numeroTelefoneDoador = phoneNumbers
+      doador.dataNascimentoDoador = dateFormat(doador.dataNascimentoDoador)
+    }
+
+    console.log(result)
+    return res.status(200).json(result)
   },
-};
+}
--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,43 @@
-const routes = require("express").Router();
-const multer = require("multer");
-const multerconfig = require("./config/multer");
-const path = require("path");
+const routes = require('express').Router()
+const multer = require('multer')
+const multerconfig = require('./config/multer')
+const path = require('path')
 
 // Middlewares
 
-const tokenValidate = require("./middlewares/tokenValidate");
-routes.use(tokenValidate);
+const tokenValidate = require('./middlewares/tokenValidate')
+routes.use(tokenValidate)
 
 // Controllers
 
-const LoginController = require("./controllers/LoginController");
-const BancoSangueController = require("./controllers/BancoSangueController");
-const DoadoresController = require("./controllers/DoadoresController");
-const ResponsavelController = require("./controllers/ResponsavelController");
-const DoacaoController = require("./controllers/DoacaoController");
+const LoginController = require('./controllers/LoginController')
+const BancoSangueController = require('./controllers/BancoSangueController')
+const DoadoresController = require('./controllers/DoadoresController')
+const ResponsavelController = require('./controllers/ResponsavelController')
+const DoacaoController = require('./controllers/DoacaoController')
 
-routes.post("/login", LoginController.validate);
+// Routes
 
-routes.get("/bancos", BancoSangueController.indexAll);
-routes.get("/bancos/:id", BancoSangueController.index);
+routes.post('/login', LoginController.validate)
 
-routes.get("/doacao/:idDoacao/:idDoador", DoacaoController.index);
+routes.get('/bancos', BancoSangueController.indexAll)
+routes.get('/bancos/:id', BancoSangueController.index)
 
-routes.get("/doadores", DoadoresController.indexAll);
-routes.get("/doadores/:id", DoadoresController.index);
+routes.get('/doacao/:idDoacao/:idDoador', DoacaoController.index)
 
-routes.get("/responsavel/:id", ResponsavelController.index);
+routes.get('/doadores', DoadoresController.indexAll)
+routes.get('/doadores/:id', DoadoresController.index)
 
-routes.post("/imagens", multer(multerconfig).single("image"), (req, res) =>
+routes.get('/responsavel/:id', ResponsavelController.index)
+
+routes.post('/imagens', multer(multerconfig).single('image'), (req, res) =>
   res.json({ sucess: true })
-);
+)
 
 // API Services
 
-const SendMailer = require("./services/sendmailer/SendMailer");
+const SendMailer = require('./services/sendmailer/SendMailer')
 
-routes.post("/sendmail", SendMailer.sendmail);
+routes.post('/sendmail', SendMailer.sendmail)
 
-module.exports = routes;
+module.exports = routes
