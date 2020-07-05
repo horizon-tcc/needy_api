3fa5aeb6c0b1d907297f5683cd0155efb9f41293
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-07-05 03:53:27 -0300

    bugfix

--- src/controllers/BancoSangueController.js
+++ src/controllers/BancoSangueController.js
@@ -2,22 +2,34 @@ const database = require('../database/database')
 
 module.exports = {
   async indexAll(req, res) {
-
     const [count] = await database('tbBancoSangue').count()
+    res.header('X-Total-Count', count['count(*)'])
+
     const result = await database('tbBancoSangue').select('*')
 
-    res.header('X-Total-Count', count['count(*)'])
+    for(const bancoSangue of result) {
+      const phoneNumbers = await database('tbTelefoneBancoSangue')
+        .where('idBancoSangue', bancoSangue.idBancoSangue)
+        .pluck('numeroTelefoneBanco')
+
+      bancoSangue.numeroTelefoneBanco = phoneNumbers
+    }
+
     return res.json(result)
   },
 
   async index(req, res) {
-    const {
-      id
-    } = req.params
+    const { id } = req.params
     const result = await database('tbBancoSangue')
       .where('idBancoSangue', id)
       .select('*')
       .first()
+
+    const phoneNumbers = await database('tbTelefoneBancoSangue')
+      .where('idBancoSangue', id)
+      .pluck('numeroTelefoneBanco')
+
+    result.numeroTelefoneBanco = phoneNumbers
 
     return res.json(result)
   }
