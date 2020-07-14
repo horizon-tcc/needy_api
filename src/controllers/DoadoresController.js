558b15f80ca893f5618657976078f5b3146c0851
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-07-14 16:24:58 -0300

    atualizando status do doador na primeira vez que acessa o app

--- src/controllers/DoadoresController.js
+++ src/controllers/DoadoresController.js
@@ -1,12 +1,21 @@
 const database = require('../database/database')
-const { dateFormat } = require('../utils/dateFormat')
+const {
+  dateFormat
+} = require('../utils/dateFormat')
 const isValidPass = require('../utils/validations/password')
 
 module.exports = {
   async index(req, res) {
 
-    const doador = await database('tbDoador')
+    const {
+      idDoador
+    } = await database('tbDoador')
       .where('tbDoador.idUsuario', req.idUsuario)
+      .select('idDoador')
+      .first()
+
+    const doador = await database('tbDoador')
+      .where('tbDoador.idDoador', idDoador)
       .join('tbSexo', {
         'tbSexo.idSexo': 'tbDoador.idSexo',
       })
@@ -20,7 +29,6 @@ module.exports = {
         'tbUsuario.idUsuario': 'tbDoador.idUsuario'
       })
       .select([
-        // 'tbDoador.*',
         'tbDoador.idDoador',
         'tbDoador.nomeDoador',
         'tbDoador.dataNascimentoDoador',
@@ -34,7 +42,7 @@ module.exports = {
         'tbDoador.cidadeDoador',
         'tbDoador.ufDoador',
         'tbDoador.idUsuario',
-        'tbDoador.statusDoador',//
+        'tbDoador.statusDoador',
         'tbSexo.descricaoSexo',
         'tbFatorRh.descricaoFatorRh',
         'tbTipoSanguineo.descricaoTipoSanguineo',
@@ -45,7 +53,7 @@ module.exports = {
       .first()
 
     const phoneNumbers = await database('tbTelefoneDoador')
-      .where('tbTelefoneDoador.idDoador', 1)
+      .where('tbTelefoneDoador.idDoador', idDoador)
       .pluck('tbTelefoneDoador.numeroTelefoneDoador')
 
     doador.numeroTelefoneDoador = phoneNumbers
@@ -56,22 +64,87 @@ module.exports = {
 
   async update(req, res) {
 
-    if(!isValidPass(req.body.novaSenha))
-      return res.status(400).json({ error: 'Password malformatted!' })
+    async function changeStatusFirstOpenApp(req) {
+      const {
+        statusDoador
+      } = await database('tbDoador')
+        .where('tbDoador.idUsuario', req.idUsuario)
+        .select('statusDoador')
+        .first()
 
-    database('tbUsuario')
+      if (statusDoador == 1)
+        await database('tbDoador')
+        .where('tbDoador.idUsuario', req.idUsuario)
+        .update({
+          statusDoador: 2
+        })
+    }
+
+    changeStatusFirstOpenApp(req)
+
+    if (!isValidPass(req.body.novaSenha))
+      return res.status(400).json({
+        error: 'Password malformatted!'
+      })
+
+    await database('tbUsuario')
       .where('tbUsuario.idUsuario', req.idUsuario)
       .update({
         senhaUsuario: req.body.novaSenha
       })
-      .then(() => {
-        res.status(200)
-        res.json({ success: true })
+
+       const {
+      idDoador
+    } = await database('tbDoador')
+      .where('tbDoador.idUsuario', req.idUsuario)
+      .select('idDoador')
+      .first()
+
+    const doador = await database('tbDoador')
+      .where('tbDoador.idDoador', idDoador)
+      .join('tbSexo', {
+        'tbSexo.idSexo': 'tbDoador.idSexo',
       })
-      .catch(err => {
-        console.log(err)
-        res.status(400)
-        return res.json({ error: err.message })
+      .join('tbFatorRh', {
+        'tbFatorRh.idFatorRh': 'tbDoador.idFatorRh',
       })
+      .join('tbTipoSanguineo', {
+        'tbTipoSanguineo.idTipoSanguineo': 'tbDoador.idTipoSanguineo',
+      })
+      .join('tbUsuario', {
+        'tbUsuario.idUsuario': 'tbDoador.idUsuario'
+      })
+      .select([
+        'tbDoador.idDoador',
+        'tbDoador.nomeDoador',
+        'tbDoador.dataNascimentoDoador',
+        'tbDoador.cpfDoador',
+        'tbDoador.rgDoador',
+        'tbDoador.logradouroDoador',
+        'tbDoador.bairroDoador',
+        'tbDoador.cepDoador',
+        'tbDoador.numeroEndDoador',
+        'tbDoador.complementoEndDoador',
+        'tbDoador.cidadeDoador',
+        'tbDoador.ufDoador',
+        'tbDoador.idUsuario',
+        'tbDoador.statusDoador',
+        'tbSexo.descricaoSexo',
+        'tbFatorRh.descricaoFatorRh',
+        'tbTipoSanguineo.descricaoTipoSanguineo',
+        'tbUsuario.emailUsuario',
+        'tbUsuario.fotoUsuario',
+        'tbUsuario.statusUsuario'
+      ])
+      .first()
+
+    const phoneNumbers = await database('tbTelefoneDoador')
+      .where('tbTelefoneDoador.idDoador', idDoador)
+      .pluck('tbTelefoneDoador.numeroTelefoneDoador')
+
+    doador.numeroTelefoneDoador = phoneNumbers
+    doador.dataNascimentoDoador = dateFormat(doador.dataNascimentoDoador)
+
+    return res.status(200).json(doador)
   }
 }
