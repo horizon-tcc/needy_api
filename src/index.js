5466622ff4ff04a154922ded6d3e83bfc6b6f074
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-07-13 21:43:24 -0300

    bugfix: pegando arquivos estaticos

--- src/index.js
+++ src/index.js
@@ -14,6 +14,6 @@ app.use(cors())
 app.use(routes)
 
 app.use('/imagens',
- express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
+ express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(process.env.PORT || 4000)
