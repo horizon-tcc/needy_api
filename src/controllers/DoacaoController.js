6b6226a9ac208210a9420be4f8f981957d82e08c
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-07-16 00:20:57 -0300

    adicionando percentual para doar novamente

--- src/controllers/DoacaoController.js
+++ src/controllers/DoacaoController.js
@@ -1,5 +1,8 @@
 const database = require("../database/database")
-const { dateFormat } = require('../utils/dateFormat')
+const {
+  dateFormat
+} = require('../utils/dateFormat')
+const moment = require('moment')
 
 module.exports = {
   async indexAll(req, res) {
@@ -32,76 +35,98 @@ module.exports = {
       .where('tbDoacao.idDoador', req.idDoador)
       .orderBy('tbDoacao.dataDoacao', 'desc')
 
-      for(const doacao of doacoes) {
-        let date = dateFormat(doacao.dataDoacao)
-        doacao.dataDoacao = date
+    const gender = await database('tbDoador')
+      .where('tbDoador.idDoador', req.idDoador)
+      .join('tbSexo', {
+        'tbDoador.idSexo': 'tbSexo.idSexo'
+      })
+      .select('tbSexo.descricaoSexo')
+      .first()
 
-        const months = [
-          'jan',
-          'mar',
-          'abr',
-          'mai',
-          'jun',
-          'jul',
-          'ago',
-          'set',
-          'out',
-          'nov',
-          'dez'
-        ]
+    for (const doacao of doacoes) {
+      // get percent to blood reload
+      const presentDate = moment()
+      const lastDate = moment(doacao.dataDoacao)
 
-        let month = date.split('/')[1]
-        let monthFormatted = undefined
-        switch(month) {
-          case '01':
-            monthFormatted = 'jan'
-            break
-          case '02':
-            monthFormatted = 'fev'
-            break
-          case '03':
-            monthFormatted = 'mar'
-            break
-          case '04':
-            monthFormatted = 'abr'
-            break
-          case '05':
-            monthFormatted = 'mai'
-            break
-          case '06':
-            monthFormatted = 'jun'
-            break
-          case '07':
-            monthFormatted = 'jul'
-            break
-          case '08':
-            monthFormatted = 'ago'
-            break
-          case '09':
-            monthFormatted = 'set'
-            break
-          case '10':
-            monthFormatted = 'out'
-            break
-          case '11':
-            monthFormatted = 'nov'
-            break
-          case '12' :
-            monthFormatted = 'dez'
-            break
-        }
+      const diffDays = presentDate.diff(lastDate, 'days')
 
-        let dateFormatted = date.split('/')
-        dateFormatted.pop()
-        dateFormatted[1] = monthFormatted
+      const BLOODRELOAD_DAYS = (gender.descricaoSexo === 'Masculino') ? 60 : 90
 
-        doacao.dataDoacaoFormatted = `${dateFormatted[0]}/${dateFormatted[1]}`
+      const percent = Math.floor(diffDays / BLOODRELOAD_DAYS * 100)
 
-        if(doacao.descricaoUnidadeMedida === 'Mililitro (ml)') {
-          doacao.totalDoacao = 400 / 1000
-          doacao.descricaoUnidadeMedida = 'Litros (l)'
-        }
+      doacao.bloodReloadPercent = (percent > 100) ? 100 + '%' : percent + '%'
+      //
+
+      let date = dateFormat(doacao.dataDoacao)
+      doacao.dataDoacao = date
+
+      const months = [
+        'jan',
+        'mar',
+        'abr',
+        'mai',
+        'jun',
+        'jul',
+        'ago',
+        'set',
+        'out',
+        'nov',
+        'dez'
+      ]
+
+      let month = date.split('/')[1]
+      let monthFormatted = undefined
+      switch (month) {
+        case '01':
+          monthFormatted = 'jan'
+          break
+        case '02':
+          monthFormatted = 'fev'
+          break
+        case '03':
+          monthFormatted = 'mar'
+          break
+        case '04':
+          monthFormatted = 'abr'
+          break
+        case '05':
+          monthFormatted = 'mai'
+          break
+        case '06':
+          monthFormatted = 'jun'
+          break
+        case '07':
+          monthFormatted = 'jul'
+          break
+        case '08':
+          monthFormatted = 'ago'
+          break
+        case '09':
+          monthFormatted = 'set'
+          break
+        case '10':
+          monthFormatted = 'out'
+          break
+        case '11':
+          monthFormatted = 'nov'
+          break
+        case '12':
+          monthFormatted = 'dez'
+          break
       }
+
+      let dateFormatted = date.split('/')
+      dateFormatted.pop()
+      dateFormatted[1] = monthFormatted
+
+      doacao.dataDoacaoFormatted = `${dateFormatted[0]}/${dateFormatted[1]}`
+
+      if (doacao.descricaoUnidadeMedida === 'Mililitro (ml)') {
+        doacao.totalDoacao = 400 / 1000
+        doacao.descricaoUnidadeMedida = 'Litros (l)'
+      }
+
+    }
 
 
     return res.status(200).json(doacoes)
